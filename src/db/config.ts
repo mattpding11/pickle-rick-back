6a09ab903e512d3ed1cbe8e4573bddfcf241227e
Mateo Peñaranda <mateo_1999_7@outlook.es>
2023-01-13 02:40:24 -0500

    Update config.ts

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,18 +2,20 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
 
-    // const host = {
-    //     local: "127.0.0.1",
-    //     docker: "mongo"
-    // }
+    const host = {
+        local: "127.0.0.1",
+        docker: "mongo"
+    }
 
-    const mongo_cnn = "mongodb://mongo:WQcjY0T1IpnEJ5TD0wtC@containers-us-west-35.railway.app:5880"
+    const mongo_cnn = `mongodb://${host.docker}/pickle_rick_db`
+
+    const mongo_cnn_prod = "mongodb://mongo:WQcjY0T1IpnEJ5TD0wtC@containers-us-west-35.railway.app:5880"
 
     try {
 
         mongoose.set("strictQuery", false);
 
-        await mongoose.connect(mongo_cnn, {
+        await mongoose.connect(mongo_cnn_prod, {
             useNewUrlParser: true,
         });
 
